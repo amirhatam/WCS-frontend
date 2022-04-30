941762f41019fb1f013bd3b38cc8ed1a0da945a5
Amir <amirhatam136@gmail.com>
2022-04-30 15:39:42 +0200

    init frontend projet

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { MDBContainer } from 'mdb-react-ui-kit';
+
+function App() {
+  return (
+    <MDBContainer fluid>
+      Start
+    </MDBContainer>
+  );
+}
+
+export default App;
