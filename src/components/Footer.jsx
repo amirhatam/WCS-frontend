6f5e3aacdc00fa13c6951b5f055bb56d730426c0
Amir <amirhatam136@gmail.com>
2022-04-30 16:04:44 +0200

    add footer to projet

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../assets/styles/Footer.css'
+
+
+export default function Footer() {
+    return (
+        <footer>
+            <p>Réalisé par Jason en Anthestérion de l'an 515 avant JC</p>
+        </footer>
+    )
+}
