5783d523574d7741024eef33e3958302023e2d47
Amir <amirhatam136@gmail.com>
2022-04-30 18:10:53 +0200

    install axios, send & read data Done

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,15 +1,64 @@
-import { MDBBtn, MDBCardTitle, MDBCol, MDBInput, MDBRow } from 'mdb-react-ui-kit'
-import React from 'react'
+import { MDBBtn, MDBCardTitle, MDBCol, MDBContainer, MDBInput, MDBRow } from 'mdb-react-ui-kit'
+import React, { useState, useEffect } from 'react';
+import axios from "axios";
+
 
 export default function Main() {
+    const [input, setInput] = useState("")
+    const [argonaute, setArgonaute] = useState([]);
+
+
+
+    const getInputValue = (e) => {
+        const userValue = e.target.value.toLowerCase()
+        setInput(userValue)
+    }
+
+    const addArgonaute = async () => {
+        try {
+            const response = await axios.post('http://localhost:8080/argonaute/add', { argonaute: input })
+            const responseData = await axios.get("http://localhost:8080/argonaute")
+            setArgonaute(responseData.data)
+
+            if (!response) {
+                console.error("response err:", response);
+            }
+
+
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+
+
+    useEffect(() => {
+        (async () => {
+            try {
+                const response = await axios.get("http://localhost:8080/argonaute")
+
+                if (response.status === 200) {
+                    // console.log(response.data);
+                    setArgonaute(response.data)
+
+                }
+
+            } catch (err) {
+                console.error(err);
+            }
+        })();
+    }, [])
+
+    // console.log(argonaute);
+
     return (
-        <main className='text-center'>
+        <MDBContainer className='text-center'>
             <MDBCardTitle className='my-5 display-6'>Ajouter un(e) Argonaute</MDBCardTitle>
-            <MDBRow className="input-group justify-content-center my-5">
-                <MDBCol size='3' className="input-group-prepend px-0">
+            <MDBRow className="input-group justify-content-center pb-5 block-example border-bottom border-gray">
+                <MDBCol size='2' className="input-group-prepend px-0">
                     <MDBInput
                         label='Ajouter votre Argonaute'
-                        // onChange={getInputValue}
+                        onChange={getInputValue}
                         id='form'
                         type='text'
                         className='mb-md-3 form-control'
@@ -18,7 +67,7 @@ export default function Main() {
 
                 <MDBCol size='1' className="input-group-prepend px-0">
                     <MDBBtn id="btn"
-                    // onClick={props.findMysteryWord}
+                        onClick={addArgonaute}
                     >
                         Envoyer
                     </MDBBtn>
@@ -27,11 +76,25 @@ export default function Main() {
 
             <MDBCardTitle className='my-5 display-6'>Membres de l'équipage</MDBCardTitle>
 
-            <section class="member-list">
-                <div class="member-item">Eleftheria</div>
-                <div class="member-item">Gennadios</div>
-                <div class="member-item">Lysimachos</div>
-            </section>
-        </main>
+            <MDBContainer>
+                <MDBRow>
+                    {
+                        argonaute.map((e, i) => {
+                            return (
+                                <MDBCol
+                                    size='4'
+                                    key={i}
+                                    className="text-capitalize my-3"
+                                >
+                                    <MDBCardTitle className='fw-light'>
+                                        {e.argonaute}
+                                    </MDBCardTitle>
+                                </MDBCol>
+                            )
+                        })
+                    }
+                </MDBRow>
+            </MDBContainer>
+        </MDBContainer>
     )
 }
